3a845eb0fac808785e1974a4179e0310d8372855
samashtar <sam.ashtar@flatironschool.com>
2019-03-24 17:59:09 -0400

    scraped title into csv

--- scrape.js
+++ scrape.js
@@ -1,17 +1,27 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const fs = require("fs");
+const writeStream = fs.createWriteStream("scrape.csv");
+
+//write headers
+writeStream.write(`postTitle`);
 
 request(
   "https://artistshortcut.com/30-effective-ways-promote-music/",
   (err, res, html) => {
     if (!err && res.statusCode == 200) {
-      //   console.log(html);
       const $ = cheerio.load(html);
 
-      const siteHeading = $(".yoast-breadcrumbs");
-
-      const output = siteHeading.find("h1").text();
-      console.log(output);
+      const postTitle = $(".newh2").each((i, el) => {
+        const title = $(el)
+          .find(".ez-toc-section")
+          .text()
+          .replace(/\s\\s+/g, "");
+        console.log(title);
+        // write to csv
+        writeStream.write(`${title}`);
+      });
+      console.log("scrape done");
     }
   }
 );
